f34abeb860fcf952fa5011246dbb055fc623b57f
KentonDunn <kenton.dunn@gmail.com>
2018-06-16 22:12:53 -0400

    I've done the best I can, but I really need more time and help with this one.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
     //A POST routes /api/friends. This will be used to handle incoming survey results. 
     //This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function (req, res) {
-        res.json(friendsArray);
+        friendsArray.push(req.body);
+        console.log(friendsArray);
     })
 }
